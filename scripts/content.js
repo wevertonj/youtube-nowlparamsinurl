0e93249c0390dfdb3ec9ca54270df6357392d5dc
Weverton J da Silva <weverton@getlayer.com>
2022-04-09 09:50:09 -0300

    Fix to work when URL is changed without reloading the page and included thumbs links.

--- scripts/content.js
+++ scripts/content.js
@@ -1,32 +1,49 @@
 window.onload = function () {
     function ytwlRemoveParams() {
-        document.querySelectorAll('a.ytd-playlist-video-renderer').forEach(function (link) {
+        document.querySelectorAll('a.yt-simple-endpoint').forEach(function (link) {
             let href = link.href;
 
-            if (!link.classList.contains('ytwl-removed-params')) {
-                if (href.indexOf('&list=WL') > -1) {
-                    let matches = href.match(/&index=([^&]*)/);
-                    if (matches && matches.length > 1) {
-                        href = href.replace(matches[0], '');
-                    }
-
-                    href = href.replace('&list=WL', '');
-                    link.href = href;
-                    link.classList.add('ytwl-removed-params');
+            if (href.indexOf('&list=WL') > -1) {
+                let matches = href.match(/&index=([^&]*)/);
+                if (matches && matches.length > 1) {
+                    href = href.replace(matches[0], '');
                 }
+
+                href = href.replace('&list=WL', '');
+                link.href = href;
+                link.classList.add('ytwl-removed-params');
             }
         });
     }
 
-    if (window.location.href == 'https://www.youtube.com/playlist?list=WL') {
-        let ytwlTimer = setInterval(() => {
-            let ytwlVideoContainer = document.querySelector('ytd-playlist-video-list-renderer');
+    function ytwlLauncher() {
+        if (window.location.href == 'https://www.youtube.com/playlist?list=WL') {
+            let ytwlTimer = setInterval(() => {
+                let ytwlVideoContainer = document.querySelector('ytd-playlist-video-list-renderer');
 
-            if (ytwlVideoContainer) {
-                ytwlVideoContainer.addEventListener('DOMSubtreeModified', () => ytwlRemoveParams());
-                ytwlRemoveParams();
-                clearInterval(ytwlTimer);
-            }
-        }, 500);
+                if (ytwlVideoContainer) {
+                    ytwlVideoContainer.addEventListener('DOMSubtreeModified', () => ytwlRemoveParams());
+                    ytwlRemoveParams();
+                    clearInterval(ytwlTimer);
+                }
+            }, 500);
+        }
     }
+
+    function ytwlUrlChangeObserver() {
+        let lastUrl = location.href;
+        new MutationObserver(() => {
+            const url = location.href;
+            if (url !== lastUrl) {
+                lastUrl = url;
+                ytwlLauncher();
+            }
+        }).observe(document, {
+            subtree: true,
+            childList: true
+        });
+    }
+
+    ytwlUrlChangeObserver();
+    ytwlLauncher();
 }
